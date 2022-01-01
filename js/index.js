15fc44675717f5748cb42340eaeb18d5d13c26a1
nefetina <nefetinaira.tomas@gsfe.tupcavite.edu.ph>
2022-01-01 12:52:01 +0800

    Add Files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+var email = document.getElementById("email").value;
+var pass = document.getElementById("pass").value;
+
+function check() {
+    if (email) {
+        console.log(email);
+    } else {
+        console.log("wala")
+    }
+}
