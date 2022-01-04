3946b7d14ad2fa835f4a9835e162c927b3ea5d01
CMiole <chrichelle.miole@gsfe.tupcavite.edu.ph>
2022-01-04 21:47:25 +0800

    Add Files

--- js/application.js
+++ js/application.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
 
-    $('#signup').click(function() {
+    $('#pindot').click(function() {
         var fname = $('#firstname').val();
         var sname = $('#surname').val();
         var course = $('#cs').val();
         var age = $('#age').val();
         var position = $('#pos').val();
         var parte = $('#party').val();
+        var desc = $('#desc')
 
-        var applications = [fname, sname, course, age, position, parte];
+        var applications = "<tr><td>" + fname + "</td><td>" + surname + "</td><td>" + course + "</td><td>" + age + "</td><td>" + position + "</td><td>" +
+            position + "</td><td>" + parte + "</td><td>";
 
-        console.log(applications)
+        $('#attendanceTable').append(applications);
+
+        console.log(fname);
+        console.log(sname);
+        console.log(course);
+        console.log(age);
+        console.log(position);
+        console.log(parte);
+        console.log(desc);
+        console.log(applications);
+
     })
-
-
-
 
 })
--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
-var email = document.getElementById("email").value;
-var pass = document.getElementById("pass").value;
+$(document).ready(function() {
 
-function check() {
-    if (email) {
-        console.log(email + pass);
-    } else {
-        console.log("wala");
-    }
-}
+    $('#bb').click(function() {
+        username = $('#email').val();
+        password = $('#pass').val();
+
+        if (username == "admin@gsfe.tupcavite.edu.ph" && password == "admin123") {
+            location.href = "homepage.html";
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Username and/or Password is incorrect',
+                text: 'Something went wrong!',
+                confirmButtonColor: '#A42222',
+                confirmButtonText: 'Okay'
+            });
+        }
+    })
+
+})
